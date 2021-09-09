30c4a0a5bbcdd5c88cdb9d4232fb8490915a5029
Marinus Klasen <marinus@mklasen.nl>
2021-09-09 15:00:01 +0200

    Automatically set new version to 5.4.2.9

--- premia-demo.php
+++ premia-demo.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Premia Demo
  * Plugin URI:        https://getpremia.com/premia-demo
  * Description:       Premia Demo
- * Version:           5.4.2.8
+ * Version:           5.4.2.9
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Marinus Klasen
