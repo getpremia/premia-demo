34daa07585ca77a8777d439a871ad008dff7d9ce
Marinus Klasen <marinus@mklasen.nl>
2021-10-24 09:00:01 +0200

    Automatically set new version to 7.5.7.7

--- premia-demo.php
+++ premia-demo.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Premia Demo
  * Plugin URI:        https://getpremia.com/premia-demo
  * Description:       Premia Demo
- * Version:           7.5.7.6
+ * Version:           7.5.7.7
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Marinus Klasen
